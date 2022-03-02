b1896c3ff41fe954f93ced1fd8aba422dd763839
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-03-01 23:57:01 -0300

    menu hamburguer (done)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import './style.css';
 
 function Header() {
+  const [checkbox, setCheckbox] = useState(false);
+  const teste = () => {
+    setCheckbox(!checkbox);
+    console.log('foi');
+  }
   return (
-    <header>
-      <label className="menu-hamburger" htmlFor="menu-hamburger">
-        <GiHamburgerMenu />
-      </label>
-      <input className="checkbox-menu" type="checkbox" id="menu-hamburger"/>
-    <nav className="navigation">
-        <p>oi</p>
-        <p>oi</p>
-        <p>oi</p>
-        <p>oi</p>
-        <p>oi</p>
-        <p>oi</p>
-        <p>oi</p>
-    </nav>
-    oi
+    <header className="header">
+      <nav className="navigation">
+        <label onClick={ () => teste() } htmlFor="checbox-menu" className="checbox-menu">
+          <GiHamburgerMenu />
+        </label>
+        <input className="input-checkbox" type="checkbox" id="checbox-menu" />
+        <ul className="menu-links">
+          <li>oi</li>
+          <li>eeee</li>
+          <li>teste</li>
+          <li>duduududu</li>
+          <div onClick={ () => teste() }>oi</div>
+        </ul>
+        
+        <button className="btn-theme">alterar thema</button>
+      </nav>
     </header>
     
   )
