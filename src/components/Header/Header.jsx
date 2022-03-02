5f5d75a9ea6bbb99bf151af9ed792d941c858d7c
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-03-01 22:16:16 -0300

    checkpoint header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import './style.css';
+
+function Header() {
+  return (
+    <header>
+      <label className="menu-hamburger" htmlFor="menu-hamburger">
+        <GiHamburgerMenu />
+      </label>
+      <input className="checkbox-menu" type="checkbox" id="menu-hamburger"/>
+    <nav className="navigation">
+        <p>oi</p>
+        <p>oi</p>
+        <p>oi</p>
+        <p>oi</p>
+        <p>oi</p>
+        <p>oi</p>
+        <p>oi</p>
+    </nav>
+    oi
+    </header>
+    
+  )
+}
+
+export default Header
