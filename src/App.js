152f6c439a00e716110730f80295abff9e233042
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-03-01 21:29:56 -0300

    definindo as cores do site

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const alterar = () => {
+    document.body.classList.toggle('dark-theme');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <header>oidddddddddd</header>
+      <p className="selected">ola</p>
+      <p>ola</p>
+      <p>ola</p>
+      <p>ola</p>
+      <p>ola</p>
+
+      <button onClick={ () => alterar() }>alterar</button>
+    </div>  
   );
 }
 
