44f8772a981293aa586e30b5006884fcf2a118f1
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-03-03 15:34:23 -0300

    estilo de todos os tamanhos do container about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,7 +3,7 @@ import './style.css';
 
 function About() {
   return (
-    <section id="#about" className="about-container">
+    <section id="about" className="about-container">
       <div className="profile-picture" />
       <p className="about-text">
         I am 19 years old, born in São Paulo-Brazil, and currently living in Itanhaém-Brasil, on the coast of São Paulo. I like video games, listening to music and programming. I am currently studying Web Development at Trybe!! A school that teaches programming, learning and working! I am a Front-End developer and future Back-End developer.
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -15,7 +15,7 @@ function Home() {
   }
 
   return (
-    <section id="#home" className="home-container">
+    <section id="home" className="home-container">
       <h1 className="text-home">Hello, I'am Matheus Ferreira  <br /> and this is my portfolio</h1>
 
       {/* Link: https://github.com/PetzingerLucas/Portfolio/blob/master/src/pages/Home.jsx */}
