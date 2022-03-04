e3b5dafc325ecf67addf13256aa9bf368d0db453
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-03-04 13:45:15 -0300

    trocando filtro blur por opacity

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home/Home';
 import About from './components/About/About';
+import Skills from './components/Skills/Skills';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <main className="main">
         <Home />
         <About />
+        <Skills />
       </main>
     </BrowserRouter>
   );
---
+++ src/components/Skills/Skills.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+function Skills() {
+  return (
+    <section id="skills" className="skills-container">
+      ola  
+    </section>
+  )
+}
+
+export default Skills;
