736d85a1d6a48549e9dfec3742c6a2385104f5a6
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-03-27 16:34:32 -0300

    adicionei o eslint no projeto

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,37 +1,41 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
+import Slider from 'react-slick';
+import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
 import projects from '../../data';
 import './style.css';
 import i18n from '../../translate/i18n';
 import notebook from '../../images/notebook.png';
-import Slider from 'react-slick';
 import CardProject from '../CardProject/CardProject';
-import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
 
 function Projects() {
   const [imgIndex, setImgIndex] = useState(0);
 
-  function PrevArrow({ onClick }) {
-    return (
-      <button className="button-prev" onClick={ onClick }>
-        <IoIosArrowBack />
-      </button>
-    );
-  }
-  
-  function NextArrow({ onClick }) {
-    return (
-      <button className="button-next" onClick={ onClick }>
-        <IoIosArrowForward />
-      </button>
-    );
-  }
+  const PrevArrow = useCallback(({ onClick }) => (
+    <button
+      type="button"
+      className="button-prev"
+      onClick={ onClick }
+    >
+      <IoIosArrowBack />
+    </button>
+  ));
+
+  const NextArrow = useCallback(({ onClick }) => (
+    <button
+      type="button"
+      className="button-next"
+      onClick={ onClick }
+    >
+      <IoIosArrowForward />
+    </button>
+  ));
 
   const settings = {
-    className: "center",
+    className: 'center',
     focusOnSelect: true,
     centerMode: true,
     infinite: true,
-    centerPadding: "20%",
+    centerPadding: '20%',
     dots: true,
     speed: 300,
     nextArrow: <NextArrow />,
@@ -41,53 +45,52 @@ function Projects() {
       {
         breakpoint: 600,
         settings: {
-          centerPadding: "10%",
+          centerPadding: '10%',
           dots: false,
-        }
+        },
       },
       {
         breakpoint: 768,
         settings: {
-          centerPadding: "15%",
+          centerPadding: '15%',
           dots: false,
-        }
+        },
       },
       {
         breakpoint: 992,
         settings: {
-          centerPadding: "25%",
+          centerPadding: '25%',
           dots: true,
-        }
+        },
       },
-    ]
+    ],
   };
 
   return (
     <section className="projects-container">
-      <h1 id="projects" className="title-container">{ i18n.t('menu.projects')}</h1>
+      <h1 id="projects" className="title-container">
+        {i18n.t('menu.projects')}
+      </h1>
       <section className="projects">
         <section className="carousel">
+          { /* eslint-disable-next-line react/jsx-props-no-spreading */ }
           <Slider {...settings}>
-            { projects.map((project, index) => (
+            {projects.map((project, index) => (
               <CardProject
-                key={ project.name }
-                imgIndex={ imgIndex }
-                index={ index }
-                name={ project.name }
-                thumb={ project.thumb }
-                technology={ project.technologyIcons }
-                gitHub={ project.urlGitHub }
-                site={ project.urlSite }
-                about={ project.about }
+                key={project.name}
+                imgIndex={imgIndex}
+                index={index}
+                name={project.name}
+                thumb={project.thumb}
+                technology={project.technologyIcons}
+                gitHub={project.urlGitHub}
+                site={project.urlSite}
+                about={project.about}
               />
-              )) }
+            ))}
           </Slider>
-        </section>        
-        <img
-          src={ notebook }
-          alt="notebook"
-          className="notebook"
-        />
+        </section>
+        <img src={notebook} alt="notebook" className="notebook" />
       </section>
     </section>
   );
