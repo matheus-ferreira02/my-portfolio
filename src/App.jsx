11ba8b7a021d8dab2dc6752a2ce33c3cf410abfb
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-03-30 19:43:32 -0300

    commit do dia

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import Header from './components/Header/Header';
 import './App.css';
 import Home from './components/Home/Home';
 import About from './components/About/About';
+import Contacts from './components/Contacts/Contacts';
 import Skills from './components/Skills/Skills';
 import Projects from './components/Projects/Projects';
-import Contacts from './components/Contacts/Contacts';
 
 function App() {
   return (
