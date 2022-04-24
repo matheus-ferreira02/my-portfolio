ecb96f5755421275b0b0eb50f7edf8b5760431ef
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-04-24 20:56:16 -0300

    crição da estrutura do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,45 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import Header from './components/Header/Header';
-import './App.css';
-import Home from './components/Home/Home';
-import About from './components/About/About';
-import Contacts from './components/Contacts/Contacts';
-import Skills from './components/Skills/Skills';
-import Projects from './components/Projects/Projects';
-import Footer from './components/Footer/Footer';
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <BrowserRouter>
-      <Header />
-      <main className="main">
-        <Home />
-        <About />
-        <Skills />
-        <Projects />
-        <Contacts />
-      </main>
-      <Footer />
-    </BrowserRouter>
-  );
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello Vite + React!</p>
+        <p>
+          <button type="button" onClick={() => setCount((count) => count + 1)}>
+            count is: {count}
+          </button>
+        </p>
+        <p>
+          Edit <code>App.jsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {' | '}
+          <a
+            className="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
+    </div>
+  )
 }
 
-export default App;
+export default App
--- src/translate/languages/index.js
+++
@@ -1,9 +0,0 @@
-import portugueseMessages from './pt';
-import englishMessages from './en';
-
-const messages = {
-  ...portugueseMessages,
-  ...englishMessages,
-}
-
-export default messages;
